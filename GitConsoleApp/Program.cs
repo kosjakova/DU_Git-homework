3267ce66dbde153d110137c8f393dbb1b5725cfc
kosjakova <viktorijabulvane@gmail.com>
2015-11-06 12:57:22 +0200

    First commit simple console app

---
+++ GitConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GitConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n Second home work: Git 2015\n Victorija Bulvane IT 3 course.\n\n Press any key to exit!");
+            Console.ReadKey();
+        }
+    }
+}
